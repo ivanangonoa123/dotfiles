42f59083e5b5329ae6e04b40e4ae5588d7c68637
ivanangonoa123 <ivan.angonoa@gmail.com>
2018-02-14 22:28:19 -0300

    added startup script

---
+++ install/linux-startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Can be run in Ubuntu by defining a startup program in "Startup Applications" using the command "sh path/to/dotfiles/linux-startup.sh"
+
+#open some programs
+google-chrome;
+
+#TODO put tmux startup here also
+guake;
+
+subl;
